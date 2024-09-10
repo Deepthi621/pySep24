2555cb72d48b2cb78112d8b5fd8e03a530f05d04
Deepthi621 <iamdeepthi621@gmail.com>
2024-09-10 09:21:25 +0530

    new update

--- QUERY1.sql
+++ QUERY1.sql
@@ -35,5 +35,34 @@ VALUES('virat','kohli'),
 -- to query virat kohli by id
 SELECT * FROM PERSON WHERE id=3;
 
+-- to query virat by name
+SELECT * FROM Person WHERE first_name='virat';
 
+-- to query all the persons
+SELECT * FROM PERSON;
 
+-- TO QUERY ALL THE PERSONS
+SELECT id, first_name,last_name FROM Person;
+
+INSERT INTO Person(first_name,last_name)
+VALUES('Maheswaran','G');
+
+-- to update Maheswaran last name as 'Govindaraju'
+UPDATE Person
+SET last_name='Govindaraju'
+WHERE id=5;
+
+DELETE FROM Person
+WHERE id=5;
+
+-- departments
+CREATE TABLE departments
+(
+id INT PRIMARY KEY auto_increment,
+name VARCHAR(100) NOT NULL
+);
+DESC departments;
+SELECT * FROM departments;
+
+INSERT INTO departments(name)
+VALUES ('Analysis'),('Design'),('Development'),('Testing');
